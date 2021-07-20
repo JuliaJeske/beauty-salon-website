614f2b02113e4bc4f9a906c76132667c7d633eb9
JuliaJeske <juliajeske1980@gmail.com>
2021-07-19 23:16:45 -0300

    novas secoes, menu e estilizacao

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-
 //abre e fecha o menu quando clicar no icone//
 
 const nav = document.querySelector("#header nav")
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function(){
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 //quando clicar em um item do menu, esconder o menu//
 
 const links = document.querySelectorAll('nav ul li a')
-for( const link of links) {
-  link.addEventListener('click', function(){
+for (const link of links) {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
@@ -22,15 +21,44 @@ for( const link of links) {
 const header = document.querySelector("#header")
 const navHeight = header.offsetHeight
 
-window.addEventListener("scroll",function(){
-  if(window.scrollY >= navHeight) //scroll maior que a altura do header 
-  { header.classList.add('scroll')
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) //scroll maior que a altura do header 
+  {
+    header.classList.add('scroll')
 
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
   }
- 
-    else {
-      //menor que a altura do header
-      header.classList.remove('scroll')
-    }
-  
+
 })
+
+//---------- TESTIMONIALS SWIPER ------------//
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewhell: true,
+  keyboard: true
+
+});
+
+//---------- ScrolReveall------------// (mostrar elementos quando der scroll na pagina)
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duracion: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonial header, #testimonial .testimonials,
+  #contact .text, #contact .links
+  `, {
+    interval: 100
+  })
